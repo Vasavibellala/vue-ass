f62782f2bc9909bba19bd79a889ae29885eeb351
vabellala <vabellala@nisum.com>
2020-12-15 15:11:27 +0530

    vue js

---
+++ src/components/Frame.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="frame">
+    <header><h2><slot name="header">Title</slot></h2></header>
+    <h3>using slots</h3>
+    <slot>This is the default content if nothing gets specified to go here</slot>
+   
+  </div>
+  
+</template>
---
+++ src/components/MixinEx.vue
@@ -0,0 +1,15 @@
+<script>
+    import { myMixin } from './myMixin.js'
+    export default {
+      mixins: [myMixin],
+      created() {
+       console.log('Hey from the component.')
+      },
+      methods: {
+        hello() {
+          console.log('Hello everyone.')
+        }
+      }
+
+    }
+    </script>
